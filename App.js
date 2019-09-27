f64c733c4b296853b85d987a53abc069af9c5275
angdry <charian@wisetracker.co.kr>
2019-09-27 16:34:05 +0900

    firebase

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import firebase from 'react-native-firebase';
 import BlurOverlay, {
   closeOverlay,
   openOverlay,
@@ -17,6 +18,8 @@ export default class App extends Component<Props> {
   constructor(props) {
     super(props);
   }
+
+  componentDidMount = () => {};
 
   renderBlurChilds() {
     return (
@@ -47,7 +50,64 @@ export default class App extends Component<Props> {
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
-
+        {Platform.OS === 'ios' ? (
+          <Text style={styles.instructions}>
+            Press Cmd+R to reload,{'\n'}
+            Cmd+D or shake for dev menu
+          </Text>
+        ) : (
+          <Text style={styles.instructions}>
+            Double tap R on your keyboard to reload,{'\n'}
+            Cmd+M or shake for dev menu
+          </Text>
+        )}
+        <View style={styles.modules}>
+          <Text style={styles.modulesHeader}>
+            The following Firebase modules are pre-installed:
+          </Text>
+          {firebase.admob.nativeModuleExists && (
+            <Text style={styles.module}>admob()</Text>
+          )}
+          {firebase.analytics.nativeModuleExists && (
+            <Text style={styles.module}>analytics()</Text>
+          )}
+          {firebase.auth.nativeModuleExists && (
+            <Text style={styles.module}>auth()</Text>
+          )}
+          {firebase.config.nativeModuleExists && (
+            <Text style={styles.module}>config()</Text>
+          )}
+          {firebase.crashlytics.nativeModuleExists && (
+            <Text style={styles.module}>crashlytics()</Text>
+          )}
+          {firebase.database.nativeModuleExists && (
+            <Text style={styles.module}>database()</Text>
+          )}
+          {firebase.firestore.nativeModuleExists && (
+            <Text style={styles.module}>firestore()</Text>
+          )}
+          {firebase.functions.nativeModuleExists && (
+            <Text style={styles.module}>functions()</Text>
+          )}
+          {firebase.iid.nativeModuleExists && (
+            <Text style={styles.module}>iid()</Text>
+          )}
+          {firebase.links.nativeModuleExists && (
+            <Text style={styles.module}>links()</Text>
+          )}
+          {firebase.messaging.nativeModuleExists && (
+            <Text style={styles.module}>messaging()</Text>
+          )}
+          {firebase.notifications.nativeModuleExists && (
+            <Text style={styles.module}>notifications()</Text>
+          )}
+          {firebase.perf.nativeModuleExists && (
+            <Text style={styles.module}>perf()</Text>
+          )}
+          {firebase.storage.nativeModuleExists && (
+            <Text style={styles.module}>storage()</Text>
+          )}
+        </View>
         <BlurOverlay
           radius={14}
           downsampling={2}
