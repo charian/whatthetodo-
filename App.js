f8974d0d683ed0f83ca3b27c80f73d4d2dd4cc1b
angdry <charian@wisetracker.co.kr>
2019-10-02 15:34:22 +0900

    fcm

--- App.js
+++ App.js
@@ -17,7 +17,59 @@ export default class App extends React.Component {
   }
 
   async componentDidMount() {
-    //this.writeUserData('charian@naver.com', 'an', 'gihyeon');
+    this.checkPermission();
+    this.createNotificationListeners(); //add this line
+  }
+  componentWillUnmount() {
+    this.notificationListener();
+    this.notificationOpenedListener();
+  }
+  async createNotificationListeners() {
+    /*
+     * Triggered when a particular notification has been received in foreground
+     * */
+    this.notificationListener = firebase
+      .notifications()
+      .onNotification(notification => {
+        const {title, body} = notification;
+        this.showAlert(title, body);
+      });
+
+    /*
+     * If your app is in background, you can listen for when a notification is clicked / tapped / opened as follows:
+     * */
+    this.notificationOpenedListener = firebase
+      .notifications()
+      .onNotificationOpened(notificationOpen => {
+        const {title, body} = notificationOpen.notification;
+        this.showAlert(title, body);
+      });
+
+    /*
+     * If your app is closed, you can check if it was opened by a notification being clicked / tapped / opened as follows:
+     * */
+    const notificationOpen = await firebase
+      .notifications()
+      .getInitialNotification();
+    if (notificationOpen) {
+      const {title, body} = notificationOpen.notification;
+      this.showAlert(title, body);
+    }
+    /*
+     * Triggered for data only payload in foreground
+     * */
+    this.messageListener = firebase.messaging().onMessage(message => {
+      //process data message
+      console.log(JSON.stringify(message));
+    });
+  }
+  showAlert(title, body) {
+    Alert.alert(
+      title,
+      body,
+      [{text: 'OK', onPress: () => console.log('OK Pressed')}],
+      {cancelable: false},
+    );
   }
   writeUserData(email, fname, lname) {
     firebase
@@ -39,6 +91,7 @@ export default class App extends React.Component {
   }
 
   render() {
+    firebase.analytics().setCurrentScreen('Index');
     return (
       <Container>
         <Text style={styles.welcome}>What The Todo</Text>
