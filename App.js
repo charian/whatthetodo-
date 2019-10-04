e299f8c85b375e0d1044bd2c69c81add7f711374
angdry <charian@wisetracker.co.kr>
2019-10-04 16:20:33 +0900

    FCM

--- App.js
+++ App.js
@@ -1,77 +1,66 @@
-import React, {Component} from 'react';
-import {StyleSheet, Text, View, AsyncStorage} from 'react-native';
-import styled from 'styled-components';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Fragment, useEffect} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Alert,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
 import firebase from 'react-native-firebase';
-import database from 'react-native-firebase';
 
-const Container = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-  background-color: #f5fcff;
-`;
-
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      notiPermission: false,
-    };
-  }
-
-  async componentDidMount() {
+const App = () => {
+  useEffect(() => {
     this.checkPermission();
-    this.createNotificationListeners(); //add this line
-  }
-  componentWillUnmount() {
-    this.notificationListener();
-    this.notificationOpenedListener();
-  }
-  //1
-  async checkPermission() {
+    this.messageListener();
+  }, []);
+
+  checkPermission = async () => {
     const enabled = await firebase.messaging().hasPermission();
     if (enabled) {
-      this.getToken();
-      this.setState({
-        notiPermission: 'true',
-      });
-      console.log('you have already permission');
+      this.getFcmToken();
     } else {
       this.requestPermission();
-      console.log('go to get permission');
     }
-  }
+  };
 
-  //3
-  async getToken() {
-    let fcmToken = await AsyncStorage.getItem('fcmToken');
-    if (!fcmToken) {
-      fcmToken = await firebase.messaging().getToken();
-      if (fcmToken) {
-        // user has a device token
-        console.log('fcm token: ' + fcmToken);
-        await AsyncStorage.setItem('fcmToken', fcmToken);
-      }
+  getFcmToken = async () => {
+    const fcmToken = await firebase.messaging().getToken();
+    if (fcmToken) {
+      console.log(fcmToken);
+      this.showAlert('Your Firebase Token is:', fcmToken);
+    } else {
+      this.showAlert('Failed', 'No token received');
     }
-  }
+  };
 
-  //2
-  async requestPermission() {
+  requestPermission = async () => {
     try {
       await firebase.messaging().requestPermission();
       // User has authorised
-      this.getToken();
-      console.log('get permission');
     } catch (error) {
       // User has rejected permissions
-      console.log('permission rejected');
     }
-  }
+  };
 
-  async createNotificationListeners() {
-    /*
-     * Triggered when a particular notification has been received in foreground
-     * */
+  messageListener = async () => {
     this.notificationListener = firebase
       .notifications()
       .onNotification(notification => {
@@ -80,155 +69,121 @@ export default class App extends React.Component {
         this.showAlert(title, body);
       });
 
-    /*
-     * If your app is in background, you can listen for when a notification is clicked / tapped / opened as follows:
-     * */
     this.notificationOpenedListener = firebase
       .notifications()
       .onNotificationOpened(notificationOpen => {
+        console.log(notificationOpen);
         const {title, body} = notificationOpen.notification;
         this.showAlert(title, body);
       });
 
-    /*
-     * If your app is closed, you can check if it was opened by a notification being clicked / tapped / opened as follows:
-     * */
     const notificationOpen = await firebase
       .notifications()
       .getInitialNotification();
     if (notificationOpen) {
       const {title, body} = notificationOpen.notification;
+      console.log(notificationOpen);
       this.showAlert(title, body);
     }
-    /*
-     * Triggered for data only payload in foreground
-     * */
+
     this.messageListener = firebase.messaging().onMessage(message => {
-      //process data message
-      console.log('from payload : ' + JSON.stringify(message));
+      console.log(JSON.stringify(message));
     });
-  }
-  showAlert(title, body) {
+  };
+
+  showAlert = (title, message) => {
     Alert.alert(
       title,
-      body,
+      message,
       [{text: 'OK', onPress: () => console.log('OK Pressed')}],
       {cancelable: false},
     );
-  }
-  writeUserData(email, fname, lname) {
-    firebase
-      .database()
-      .ref('Users/')
-      .set({
-        email,
-        fname,
-        lname,
-      })
-      .then(data => {
-        //success callback
-        console.log('data ', data);
-      })
-      .catch(error => {
-        //error callback
-        console.log('error ', error);
-      });
-  }
+  };
 
-  render() {
-    firebase.analytics().setCurrentScreen('Index');
-    return (
-      <Container>
-        <Text style={styles.welcome}>What The Todo</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text>Notification Permission : {this.state.notiPermission}</Text>
-        <View style={styles.modules}>
-          <Text style={styles.modulesHeader}>Installed Firebase Module:</Text>
-          {firebase.admob.nativeModuleExists && (
-            <Text style={styles.module}>admob()</Text>
+  return (
+    <Fragment>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <Header />
+          {global.HermesInternal == null ? null : (
+            <View style={styles.engine}>
+              <Text style={styles.footer}>Engine: Hermes</Text>
+            </View>
           )}
-          {firebase.analytics.nativeModuleExists && (
-            <Text style={styles.module}>analytics()</Text>
-          )}
-          {firebase.auth.nativeModuleExists && (
-            <Text style={styles.module}>auth()</Text>
-          )}
-          {firebase.config.nativeModuleExists && (
-            <Text style={styles.module}>config()</Text>
-          )}
-          {firebase.crashlytics.nativeModuleExists && (
-            <Text style={styles.module}>crashlytics()</Text>
-          )}
-          {firebase.database.nativeModuleExists && (
-            <Text style={styles.module}>database()</Text>
-          )}
-          {firebase.firestore.nativeModuleExists && (
-            <Text style={styles.module}>firestore()</Text>
-          )}
-          {firebase.functions.nativeModuleExists && (
-            <Text style={styles.module}>functions()</Text>
-          )}
-          {firebase.iid.nativeModuleExists && (
-            <Text style={styles.module}>iid()</Text>
-          )}
-          {firebase.links.nativeModuleExists && (
-            <Text style={styles.module}>links()</Text>
-          )}
-          {firebase.messaging.nativeModuleExists && (
-            <Text style={styles.module}>messaging()</Text>
-          )}
-          {firebase.notifications.nativeModuleExists && (
-            <Text style={styles.module}>notifications()</Text>
-          )}
-          {firebase.perf.nativeModuleExists && (
-            <Text style={styles.module}>perf()</Text>
-          )}
-          {firebase.storage.nativeModuleExists && (
-            <Text style={styles.module}>storage()</Text>
-          )}
-        </View>
-      </Container>
-    );
-  }
-}
+          <View style={styles.body}>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Step One</Text>
+              <Text style={styles.sectionDescription}>
+                Edit <Text style={styles.highlight}>App.js</Text> to change this
+                screen and then come back to see your edits.
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>See Your Changes</Text>
+              <Text style={styles.sectionDescription}>
+                <ReloadInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Debug</Text>
+              <Text style={styles.sectionDescription}>
+                <DebugInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Learn More</Text>
+              <Text style={styles.sectionDescription}>
+                Read the docs to discover what to do next:
+              </Text>
+            </View>
+            <LearnMoreLinks />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </Fragment>
+  );
+};
 
 const styles = StyleSheet.create({
-  container: {},
-  welcome: {
-    fontSize: 30,
-    textAlign: 'center',
-    margin: 10,
+  scrollView: {
+    backgroundColor: Colors.lighter,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  engine: {
+    position: 'absolute',
+    right: 0,
   },
-  instructions2: {
-    textAlign: 'center',
-    color: 'white',
-    marginBottom: 5,
+  body: {
+    backgroundColor: Colors.white,
   },
-  logo: {
-    height: 80,
-    marginBottom: 16,
-    width: 80,
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
   },
-  modules: {
-    margin: 20,
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
   },
-  modulesHeader: {
-    fontSize: 16,
-    marginBottom: 8,
+  highlight: {
+    fontWeight: '700',
   },
-  module: {
-    fontSize: 14,
-    marginTop: 4,
-    textAlign: 'center',
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
   },
 });
+
+export default App;
